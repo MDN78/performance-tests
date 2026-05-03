0c54bd33691775048b185711be13cc6388b3fe77
Dimitri Maksimov <salesakk@gmail.com>
2026-05-03 15:36:25 +0300

    реализации gRPC API клиентов: DocumentsGatewayService

--- clients/grpc/gateway/cards/client.py
+++ clients/grpc/gateway/cards/client.py
@@ -1,17 +1,22 @@
 from grpc import Channel
+
 from clients.grpc.client import GRPCClient
 from clients.grpc.gateway.client import build_gateway_grpc_client
 from contracts.services.gateway.cards.cards_gateway_service_pb2_grpc import CardsGatewayServiceStub
-from contracts.services.gateway.cards.rpc_issue_physical_card_pb2 import IssuePhysicalCardRequest, \
+from contracts.services.gateway.cards.rpc_issue_physical_card_pb2 import (
+    IssuePhysicalCardRequest,
     IssuePhysicalCardResponse
-from contracts.services.gateway.cards.rpc_issue_virtual_card_pb2 import IssueVirtualCardRequest, \
+)
+from contracts.services.gateway.cards.rpc_issue_virtual_card_pb2 import (
+    IssueVirtualCardRequest,
     IssueVirtualCardResponse
+)
 
 
 class CardsGatewayGRPCClient(GRPCClient):
     """
     gRPC-клиент для взаимодействия с CardsGatewayService.
-    Предоставляет высокоуровневые методы для получения и создания пользователей.
+    Предоставляет высокоуровневые методы для работы с картами.
     """
 
     def __init__(self, channel: Channel):
@@ -22,14 +27,14 @@ class CardsGatewayGRPCClient(GRPCClient):
         """
         super().__init__(channel)
 
-        self.stub = CardsGatewayServiceStub(channel)  # gRPC-стаб, сгенерированный из .proto
+        self.stub = CardsGatewayServiceStub(channel)
 
     def issue_virtual_card_api(self, request: IssueVirtualCardRequest) -> IssueVirtualCardResponse:
         """
         Низкоуровневый вызов метода IssueVirtualCard через gRPC.
 
-        :param request: gRPC-запрос с ID пользователя и account ID.
-        :return: Ответ от сервиса с данными карты.
+        :param request: gRPC-запрос с ID пользователя и ID счета.
+        :return: Ответ от сервиса с данными созданной карты.
         """
         return self.stub.IssueVirtualCard(request)
 
@@ -37,28 +42,29 @@ class CardsGatewayGRPCClient(GRPCClient):
         """
         Низкоуровневый вызов метода IssuePhysicalCard через gRPC.
 
-        :param request: gRPC-запрос с ID пользователя и account ID.
-        :return: Ответ от сервиса с данными карты.
+        :param request: gRPC-запрос с ID пользователя и ID счета.
+        :return: Ответ от сервиса с данными созданной карты.
         """
         return self.stub.IssuePhysicalCard(request)
 
     def issue_virtual_card(self, user_id: str, account_id: str) -> IssueVirtualCardResponse:
         """
-        Выпуск виртуальной карты пользователю по его ID и account ID.
+        Выпуск новой виртуальной карты.
 
         :param user_id: Идентификатор пользователя.
-        :param account_id: Идентификатор аккаунта пользователя
-        :return: Ответ с информацией о карте.
+        :param account_id: Идентификатор счета.
+        :return: Ответ с информацией о созданной карте.
         """
         request = IssueVirtualCardRequest(user_id=user_id, account_id=account_id)
         return self.issue_virtual_card_api(request)
 
     def issue_physical_card(self, user_id: str, account_id: str) -> IssuePhysicalCardResponse:
         """
-        Выпуск физической карты пользователю по его ID и account ID.
+        Выпуск новой физической карты.
+
         :param user_id: Идентификатор пользователя.
-        :param account_id: Идентификатор аккаунта пользователя
-        :return: Ответ с информацией о карте.
+        :param account_id: Идентификатор счета.
+        :return: Ответ с информацией о созданной карте.
         """
         request = IssuePhysicalCardRequest(user_id=user_id, account_id=account_id)
         return self.issue_physical_card_api(request)
@@ -67,6 +73,7 @@ class CardsGatewayGRPCClient(GRPCClient):
 def build_cards_gateway_grpc_client() -> CardsGatewayGRPCClient:
     """
     Фабрика для создания экземпляра CardsGatewayGRPCClient.
-    :return: Инициализированный клиент для CardsGatewayGRPCClient.
+
+    :return: Инициализированный клиент для CardsGatewayService.
     """
     return CardsGatewayGRPCClient(channel=build_gateway_grpc_client())
